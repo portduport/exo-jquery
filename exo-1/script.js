644f8e516645426b90774c02c8ee95103df8b92f
pablo <pablo@callejo.fr>
2019-04-08 09:45:09 +0200

    Adding all EXO

---
+++ exo-1/script.js
@@ -0,0 +1,7 @@
+// Javascript vs Jquery. JQuery is a JavaScript library. With Jquery we can do the same things that we do whit js but with less code.
+console.log('exercice 1');
+// When the document is ready we show a message in the console.
+// With the '$' we are calling a JQuery code.
+$(document).ready(function(){
+	console.log('Je peux maintenant écrire du code JQuery');
+});
