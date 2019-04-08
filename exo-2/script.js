391e9e0c26d1243cc568ad29f054c0ba37820c26
pablo <pablo@callejo.fr>
2019-04-08 10:53:49 +0200

    exo-2 finished

--- exo-2/script.js
+++ exo-2/script.js
@@ -1,2 +1,14 @@
 console.log('exercice 2');
+$(document).ready(function(){
+$("#title");
+$("div.sidenav");
+$("footer");
+$("div.input-group");
+$(".glyphicon");
+$(".label-success");
+$("p.intro");
+$("span.glyphicon-time");
+$(".sidenav:first").css('background-color','red');
+$("p:odd").css('color','yellow');
 
+});
