a44b6af506eedc73fe7a361eb4407ef0d852811a
portduport <pablo@callejo.fr>
2019-04-14 21:27:33 +0200

    New branche working at home

--- exo-7/script.js
+++ exo-7/script.js
@@ -6,12 +6,14 @@ function getData() {
 
 // Pagination function for the table STARTS HERE
 
+let tableDonnes = [];
+
 function showPagination() {
-    var table = '#contact';
+    let table = '#contact';
     $('.pagination').html('');
-    var trnum = 0;
-    var maxRows = parseInt($('#maxRows').val());
-    var totalRows = $(table + ' tbody tr').length;
+    let trnum = 0;
+    let maxRows = parseInt($('#maxRows').val());
+    let totalRows = $(table + ' tbody tr').length;
     $(table + ' tr:gt(0)').each(function () {
         trnum++;
         if (trnum > maxRows) {
@@ -21,15 +23,15 @@ function showPagination() {
         }
     });
     if (totalRows > maxRows) {
-        var pagenum = Math.ceil(totalRows / maxRows);
-        for (var i = 1; i <= pagenum;) {
+        let pagenum = Math.ceil(totalRows / maxRows);
+        for (let i = 1; i <= pagenum;) {
             $('.pagination').append('<li class ="page-item" data-page="' + i + '">\<a class="page-link">' + i++ + '</a>\</li>').show();
         }
     }
     $('.pagination li:first-child').addClass('active');
     $('.pagination li').on('click', function () {
-        var pageNum = $(this).attr('data-page');
-        var trIndex = 0;
+        let pageNum = $(this).attr('data-page');
+        let trIndex = 0;
         $('.pagination li').removeClass('active');
         $(this).addClass('active');
         $(table + ' tr:gt(0)').each(function () {
@@ -43,9 +45,9 @@ function showPagination() {
     })
 }
 
-function showDATA() {
+function showDATA(table) {
     $('#donnesHERE').find('tr').remove();
-    $.each(data, function (index, value) {
+    $.each(table, function (index, value) {
         let $trCreate = $('<tr>');
         for (let register in value) {
             if (register === 'phone') {
@@ -75,186 +77,186 @@ function showDATA() {
 
 $(document).ready(function () {
 
-    var baseID = data.length;
+    let baseID = data.length;
 
-    showDATA();
+    showDATA(tableDonnes);
 
     // Remove lines STARTS HERE
     $('.remove').on('click', (event) => {
         $(event.currentTarget).closest('tr').remove();
-});
+    });
 
 
 // Adding a new contact STARTS HERE
-$("#addRow").on('click', function () {
-    let newID = baseID + 1;
-    let newContact = {
-        id: newID,
-        name:
-            {
-                first: $("#InputPrenom").val(),
-                last: $("#InputNom").val()
-            },
-        email: $("#InputEmail").val(),
-        gender: $("#GenreSelect").val(),
-        phone: $("#InputPhone").val(),
-        country: $("#CountrySelect").val()
-    };
-    data.push(newContact);
-    console.log(newContact);
-    console.log(data);
-    $('#donnesHERE').find('tr').remove();
-    showDATA();
-});
+    $("#addRow").on('click', function () {
+        let newID = baseID + 1;
+        let newContact = {
+            id: newID,
+            name:
+                {
+                    first: $("#InputPrenom").val(),
+                    last: $("#InputNom").val()
+                },
+            email: $("#InputEmail").val(),
+            gender: $("#GenreSelect").val(),
+            phone: $("#InputPhone").val(),
+            country: $("#CountrySelect").val()
+        };
+        tableDonnes.push(newContact);
+        console.log(newContact);
+        console.log(tableDonnes);
+        $('#donnesHERE').find('tr').remove();
+        showDATA(tableDonnes);
+    });
 
 // Our pagination starter
 
-$('#maxRows').on('change', function () {
-    showPagination()
-});
+    $('#maxRows').on('change', function () {
+        showPagination()
+    });
 
 
 // Our Search an filter STARTS HERE
-var $rows = $('tbody tr');
-var $searchResult = [];
-$('#search').keyup(function () {
-    $searchResult = [];
-    var val = '^(?=.*\\b' + $.trim($(this).val()).split(/\s+/).join('\\b)(?=.*\\b') + ').*$',
-        reg = RegExp(val, 'i'),
-        text;
-    console.log(reg);
-    console.log(reg.test(text));
-    // buscar en data la busqueda y devolver solo las filas que corresponden
-    // $rows.show().filter(function () {
-    //     let hide = true;
-    //     $(this).find('td').each(function () {
-    //         text = $(this).text().replace(/\s+/g, ' ');
-    //         if (reg.test(text)) {
-    //             hide = false;
-    //             $searchResult.push(text);
-    //             console.log($searchResult);
-    //         }
-    //     });
-    //     return hide;
-    // }).hide();
-    showPagination();
-});
+    let $rows = $('tbody tr');
+    let $searchResult = [];
+    $('#search').keyup(function () {
+        $searchResult = [];
+        let val = '^(?=.*\\b' + $.trim($(this).val()).split(/\s+/).join('\\b)(?=.*\\b') + ').*$',
+            reg = RegExp(val, 'i'),
+            text;
+        console.log(reg);
+        console.log(reg.test(text));
+        // buscar en data la busqueda y devolver solo las filas que corresponden
+        // $rows.show().filter(function () {
+        //     let hide = true;
+        //     $(this).find('td').each(function () {
+        //         text = $(this).text().replace(/\s+/g, ' ');
+        //         if (reg.test(text)) {
+        //             hide = false;
+        //             $searchResult.push(text);
+        //             console.log($searchResult);
+        //         }
+        //     });
+        //     return hide;
+        // }).hide();
+        showPagination();
+    });
 
 // Sort the name table STRATS HERE
-var $table = $("#contact");
-var $tableBody = $table.find("#donnesHERE");
-var rows, sortedRows, NomAZ, NomZA, sortAscendingNom;
-var $buttons = $("th a");
+    let $table = $("#contact");
+    let $tableBody = $table.find("#donnesHERE");
+    let rows, sortedRows, NomAZ, NomZA, sortAscendingNom;
+    let $buttons = $("th a");
 
-function cmp(a, b) {
-    NomAZ = $(a).find('td:first-child').text().toLowerCase();
-    NomZA = $(b).find('td:first-child').text().toLowerCase();
-    if (NomAZ < NomZA) {
-        return (sortAscendingNom) ? -1 : 1
-    } else if (NomAZ > NomZA) {
-        return (sortAscendingNom) ? 1 : -1
-    } else {
-        return 0
+    function cmp(a, b) {
+        NomAZ = $(a).find('td:first-child').text().toLowerCase();
+        NomZA = $(b).find('td:first-child').text().toLowerCase();
+        if (NomAZ < NomZA) {
+            return (sortAscendingNom) ? -1 : 1
+        } else if (NomAZ > NomZA) {
+            return (sortAscendingNom) ? 1 : -1
+        } else {
+            return 0
+        }
     }
-}
 
-function updateData() {
-    $tableBody.remove('tr');
-    $tableBody.append(sortedRows);
-}
-
-$buttons.click(function () {
-    rows = $tableBody.find("tr");
-    if ($(this).attr('id') === 'sortAscendingNom') {
-        sortAscendingNom = true
-    } else if ($(this).attr('id') === 'sortDescendingNom') {
-        sortAscendingNom = false
+    function updateData() {
+        $tableBody.remove('tr');
+        $tableBody.append(sortedRows);
     }
-    sortedRows = rows.sort(cmp);
-    updateData();
-});
+
+    $buttons.click(function () {
+        rows = $tableBody.find("tr");
+        if ($(this).attr('id') === 'sortAscendingNom') {
+            sortAscendingNom = true
+        } else if ($(this).attr('id') === 'sortDescendingNom') {
+            sortAscendingNom = false
+        }
+        sortedRows = rows.sort(cmp);
+        updateData();
+    });
 
 // Export as CSV
 
-function exportTableToCSV($table, filename) {
+    function exportTableToCSV($table, filename) {
 
-    var $rows = $table.find('tr:has(td)'),
+        let $rows = $table.find('tr:has(td)'),
 
-        // Temporary delimiter characters unlikely to be typed by keyboard
-        // This is to avoid accidentally splitting the actual contents
-        tmpColDelim = String.fromCharCode(11), // vertical tab character
-        tmpRowDelim = String.fromCharCode(0), // null character
+            // Temporary delimiter characters unlikely to be typed by keyboard
+            // This is to avoid accidentally splitting the actual contents
+            tmpColDelim = String.fromCharCode(11), // vertical tab character
+            tmpRowDelim = String.fromCharCode(0), // null character
 
-        // actual delimiter characters for CSV format
-        colDelim = '","',
-        rowDelim = '"\r\n"',
+            // actual delimiter characters for CSV format
+            colDelim = '","',
+            rowDelim = '"\r\n"',
 
-        // Grab text from table into CSV formatted string
-        csv = '"' + $rows.map(function (i, row) {
-            var $row = $(row),
-                $cols = $row.find('td');
+            // Grab text from table into CSV formatted string
+            csv = '"' + $rows.map(function (i, row) {
+                let $row = $(row),
+                    $cols = $row.find('td');
 
-            return $cols.map(function (j, col) {
-                var $col = $(col),
-                    text = $col.text();
+                return $cols.map(function (j, col) {
+                    let $col = $(col),
+                        text = $col.text();
 
-                return text.replace(/"/g, '""'); // escape double quotes
+                    return text.replace(/"/g, '""'); // escape double quotes
 
-            }).get().join(tmpColDelim);
+                }).get().join(tmpColDelim);
 
-        }).get().join(tmpRowDelim)
-            .split(tmpRowDelim).join(rowDelim)
-            .split(tmpColDelim).join(colDelim) + '"';
+            }).get().join(tmpRowDelim)
+                .split(tmpRowDelim).join(rowDelim)
+                .split(tmpColDelim).join(colDelim) + '"';
 
-    // Deliberate 'false', see comment below
-    if (false && window.navigator.msSaveBlob) {
+        // Deliberate 'false', see comment below
+        if (false && window.navigator.msSaveBlob) {
 
-        var blob = new Blob([decodeURIComponent(csv)], {
-            type: 'text/csv;charset=utf8'
-        });
-
-        // Crashes in IE 10, IE 11 and Microsoft Edge
-        // See MS Edge Issue #10396033
-        // Hence, the deliberate 'false'
-        // This is here just for completeness
-        // Remove the 'false' at your own risk
-        window.navigator.msSaveBlob(blob, filename);
-
-    } else if (window.Blob && window.URL) {
-        // HTML5 Blob
-        var blob = new Blob([csv], {
-            type: 'text/csv;charset=utf-8'
-        });
-        var csvUrl = URL.createObjectURL(blob);
-
-        $(this)
-            .attr({
-                'download': filename,
-                'href': csvUrl
+            let blob = new Blob([decodeURIComponent(csv)], {
+                type: 'text/csv;charset=utf8'
             });
-    } else {
-        // Data URI
-        var csvData = 'data:application/csv;charset=utf-8,' + encodeURIComponent(csv);
 
-        $(this)
-            .attr({
-                'download': filename,
-                'href': csvData,
-                'target': '_blank'
+            // Crashes in IE 10, IE 11 and Microsoft Edge
+            // See MS Edge Issue #10396033
+            // Hence, the deliberate 'false'
+            // This is here just for completeness
+            // Remove the 'false' at your own risk
+            window.navigator.msSaveBlob(blob, filename);
+
+        } else if (window.Blob && window.URL) {
+            // HTML5 Blob
+            let blob = new Blob([csv], {
+                type: 'text/csv;charset=utf-8'
             });
+            let csvUrl = URL.createObjectURL(blob);
+
+            $(this)
+                .attr({
+                    'download': filename,
+                    'href': csvUrl
+                });
+        } else {
+            // Data URI
+            let csvData = 'data:application/csv;charset=utf-8,' + encodeURIComponent(csv);
+
+            $(this)
+                .attr({
+                    'download': filename,
+                    'href': csvData,
+                    'target': '_blank'
+                });
+        }
     }
-}
 
 // This must be a hyperlink
-$(".export").on('click', function (event) {
-    // CSV
-    var args = [$('#home>table'), 'export.csv'];
+    $(".export").on('click', function (event) {
+        // CSV
+        let args = [$('#home>table'), 'export.csv'];
 
-    exportTableToCSV.apply(this, args);
+        exportTableToCSV.apply(this, args);
 
-    // If CSV, don't do event.preventDefault() or return false
-    // We actually need this to be a typical hyperlink
-});
+        // If CSV, don't do event.preventDefault() or return false
+        // We actually need this to be a typical hyperlink
+    });
 
 // END
 })
